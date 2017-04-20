b970dfdfff76d19f6af7f0dcce88944d60b28cd0
AIE\s171*** <talscartalcom@gmail.com>
2017-04-20 14:02:18 +0930

    With a Prefab containing GUI Items including MPH Speedometre

---
+++ Assets/sliderTextSpeed.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class sliderTextSpeed : MonoBehaviour {
+
+    public Slider cartSlider_Speed;
+    public Text cartText_Speed;
+    public Rigidbody YouAreGoingTooFast_CartSpeed;
+
+    public int speed;
+	// Use this for initialization
+	void Start ()
+    {
+        YouAreGoingTooFast_CartSpeed = gameObject.GetComponent<Rigidbody>();	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        var vel = YouAreGoingTooFast_CartSpeed.velocity;
+        speed = Mathf.RoundToInt(vel.magnitude);
+        
+        if(speed > cartSlider_Speed.maxValue)
+        { cartSlider_Speed.maxValue = speed; }
+        cartSlider_Speed.value = speed;
+        cartText_Speed.text = (speed + " MPH");
+        	
+	}
+}
