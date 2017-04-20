95a29503c48bbfc27984d3d07c9636c5139f0468
Immersive <immrsv@users.noreply.github.com>
2017-04-20 12:23:19 +0930

    Character Controller: Brakes and Jumps

---
+++ Assets/Scripts/JumpController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpController : MonoBehaviour {
+
+    public float TimeToLive = 2; // Seconds
+
+    private float TTL { get; set; }
+
+	// Use this for initialization
+	void Start () {
+        TTL = TimeToLive;
+        Debug.Log("Started Jump Life");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        TTL -= Time.deltaTime;
+
+        if (TTL < 0)
+        {
+            Destroy(this);
+            Debug.Log("Destroyed Jump");
+        }
+	}
+}
